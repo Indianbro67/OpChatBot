f38a6649226193531aae633bd1eb4ab9b5fd3fc9
Indianbro67 <133229712+Indianbro67@users.noreply.github.com>
2023-07-19 21:47:58 +0530

    Update Bikash.py

--- Bikash.py
+++ Bikash.py
@@ -10,7 +10,7 @@ import re
 API_ID = os.environ.get("API_ID", "26670684") 
 API_HASH = os.environ.get("API_HASH", "60592bded0f25a9633a8133601f2c779") 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6289264953:AAG1N1Q3_KXM-V2KB-nDb9LFlMXq9WpZPv8") 
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/?retryWrites=true&w=majority")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://Bikash:Bikashop@bikash.cbkkx4c.mongodb.net/?retryWrites=true&w=majority")
 BOT_IMAGE = os.environ.get("BOT_IMAGE", "https://telegra.ph/file/8718de4a731b087ffd12a.jpg")
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "Pooja_Service_Bot")
 OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "Black_Devil_Admin")
